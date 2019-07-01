9beeacf4fe94d3f4dd3315f28747ea4510267c14
Ismail Sunny <ismiletea@gmail.com>
2019-07-01 13:36:11 +0700

    add readme

--- App.tsx
+++ App.tsx
@@ -9,16 +9,18 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, StatusBar } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import themeConfig from './src/configs/theme';
 import BooruNavigator from './src/modules/booru/BooruNavigator';
+import theme from './src/configs/theme';
 
 export default class App extends Component {
   render() {
     return (
       <PaperProvider theme={themeConfig}>
-      <BooruNavigator />
+        <StatusBar backgroundColor={theme.colors.primary} barStyle="light-content" />
+        <BooruNavigator />
       </PaperProvider>
     );
   }
---
+++ README.md
@@ -0,0 +1,9 @@
+# Syaro
+A React Native [Moebooru](https://github.com/moebooru/moebooru) client for Android
+WARNING: This application may contains sexually explicit content that is not suitable for minors.
+
+# Screenshot
+
+# Downloads
+
+# Todo
